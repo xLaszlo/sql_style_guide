fb1886dad1fb1d85a836d49061dabbc4306a5821
xLaszlo <sragner@gmail.com>
2022-04-18 18:56:23 +0100

    Update sql_style_guide.sql

--- sql_style_guide.sql
+++ sql_style_guide.sql
@@ -20,7 +20,7 @@ SELECT
     cte_table2.column2 AS alias2,
     count(*) FILTER (WHERE cte_table1.column1 = 1) AS alias3,
     CASE                                                    -- CASE/END in own row
-        WHEN cte_table1.column1 = 1 THEN 3                 -- each condition in new row
+        WHEN cte_table1.column1 = 1 THEN 3                  -- each condition in new row
         WHEN cte_table1.column1 = 2 THEN 4
         ELSE 5
     END AS alias4,
@@ -30,7 +30,7 @@ FROM
 JOIN
     cte_table2
 ON
-    cte_table1.column1 = cte_table2.column1                -- join condition in new row
+    cte_table1.column1 = cte_table2.column1                 -- join condition in new row
 JOIN (                                                      -- bracket in same row
     SELECT                                                  -- entire query tabbed in
         *
@@ -38,11 +38,11 @@ JOIN (                                                      -- bracket in same r
         cte_table2
     ) AS table3                                             -- closing bracket and alias tabbed in
 ON
-    cte_table1.column1 = table3.column1 AND                -- each term in new row 
+    cte_table1.column1 = table3.column1 AND                 -- each term in new row 
     cte_table1.column2 = table3.column2                    
 WHERE
-    cte_table1.column1 = 1 AND                             -- each term in new row 
-    cte_table1.column2 = 2 AND                             -- focus on readability with complex conditionals
+    cte_table1.column1 = 1 AND                              -- each term in new row 
+    cte_table1.column2 = 2 AND                              -- focus on readability with complex conditionals
     cte_table1.column3 IN (1, 2, 3) AND
     cte_table1.column4 IN (
         'value1', 'value2',
